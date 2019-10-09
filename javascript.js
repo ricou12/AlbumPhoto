3cb911659f9908a8acc00313b7e98c9020c4b554
ricou <e.lanza@free.fr>
2019-10-09 22:33:44 +0200

    on apprends JS

--- javascript.js
+++ javascript.js
@@ -1,6 +1,25 @@
-function imgVenise(name){
- var rep = "img/venise/large/";
- var fullImg = document.getElementById("fullImg");
- console.log;
-//  fullImg.scr= rep + 
+// mini base de donnée
+var infoPath = {
+    nameBalise: 'Venise',
+    namePath: 'img/venise'
+    // ListImg=[
+    //     '1.jpg',
+    //     '2.jpg',
+    //     '3.jpg',
+    //     '4.jpg',
+    //     '5.jpg',
+    //     '6.jpg',
+    //     '7.jpg'
+    // ]  
+};
+
+function infoTarget() {
+    var Target = event.target;
+    // debugger;
+    console.log("Propriétés de l'objet");
+    console.log(Target);
+    console.log(Target.textContent);
+    for (value in infoPath){
+       console.log(value +' = '+infoPath[value]);
+    }
 }
